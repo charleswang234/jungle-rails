dd668af23cca89d10ea64f659d9a5c81214cba93
charleswang234 <wang.charles234@gmail.com>
2018-07-19 20:32:51 +0000

    reduced number of queries

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,9 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    # @line_item = LineItem.where(order_id: @order.id)
+    @line_items = @order.line_items.includes(:product)
+    # pp @line_items
   end
 
   def create
