94e5083caaafcda866f946a5a0356662286c2913
charleswang234 <wang.charles234@gmail.com>
2018-07-19 16:40:30 +0000

    allowed users to logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+
   private
+
+  # Make the current_user method available to views also, not just controllers:
+  helper_method :current_user
+
+  # Define the current_user method:
+  def current_user
+    # Look up the current user based on user_id in the session cookie:
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
   def cart
     # value = cookies[:cart] || JSON.generate({})
