92a89195f0cfba21d49c44d9a00b4eca5eaabe26
charleswang234 <wang.charles234@gmail.com>
2018-07-20 20:21:14 +0000

    added average ratings

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all.order(created_at: :desc)
+    @products = Product.all.order(created_at: :desc).includes(:reviews)
   end
 
   def show
@@ -9,6 +9,11 @@ class ProductsController < ApplicationController
     @reviews = @product.reviews.includes(:user)
     @logged_in = logged_in?
     @current_user = current_user
+    if @reviews.size > 0
+      @rating_avg = @reviews.average(:rating).round(2)
+    else
+      @rating_avg = "no ratings"
+    end
   end
 
 end
