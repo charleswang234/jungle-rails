ec700bc59884cf5f83fc46a1400b41f35c47e18c
charleswang234 <wang.charles234@gmail.com>
2018-07-20 18:20:34 +0000

    allows user to submit reviews form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,21 @@
 class ReviewsController < ApplicationController
   def create
-    raise "Yay, I'm here!"
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.create(review_params)
+    @review.user = current_user
+    @review.save!
+    redirect_to product_path(@product), notice: 'Review Created'
+
+    # raise "Yay, I'm here!"
+
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating,
+      )
   end
 end
